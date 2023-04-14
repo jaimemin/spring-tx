13e775ebf3dbb979b7c56562f2f98c8da6498805
jaimemin <jaimemin@gmail.com>
2023-04-14 09:12:58 +0900

    Initial commit

---
+++ src/test/java/com/tistory/jaimemin/springtx/SpringTxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimemin.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
